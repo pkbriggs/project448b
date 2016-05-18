b5e98badd1d4bb82b3c3cb3f2d09b82309fbf6a8
Patrick Briggs <patkbriggs@gmail.com>
2016-05-18 16:44:21 -0700

    Have save working with an ugly button

--- js/interaction.js
+++ js/interaction.js
@@ -308,9 +308,66 @@ function updateChartConfigValue(type, key, value, is_bar_label) {
   }
 }
 
+function enableSaveButton() {
+  d3.select("#save").on("click", function(){
+    $("#svgcanvas")[0].width = CANVAS_WIDTH;
+    $("#svgcanvas")[0].height = CANVAS_HEIGHT;
+
+    var html = d3.select("svg")
+          .attr("version", 1.1)
+          .attr("xmlns", "http://www.w3.org/2000/svg")
+          .node().parentNode.innerHTML;
+
+    //console.log(html);
+    var imgsrc = 'data:image/svg+xml;base64,'+ btoa(html);
+    // var img = '<img src="'+imgsrc+'">';
+    // d3.select("#svgdataurl").html(img);
+
+    var canvas = document.querySelector("canvas"),
+        context = canvas.getContext("2d");
+
+    var image = new Image;
+    image.src = imgsrc;
+    image.onload = function() {
+      context.drawImage(image, 0, 0);
+
+      //save and serve it as an actual filename
+      binaryblob();
+
+      var a = document.createElement("a");
+      a.download = "chart.png";
+      a.href = canvas.toDataURL("image/png");
+
+       var pngimg = '<img src="'+a.href+'">';
+         d3.select("#pngdataurl").html(pngimg);
+
+      a.click();
+    };
+
+  });
+}
+
+function binaryblob(){
+  var byteString = atob(document.querySelector("canvas").toDataURL().replace(/^data:image\/(png|jpg);base64,/, "")); //wtf is atob?? https://developer.mozilla.org/en-US/docs/Web/API/Window.atob
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var dataView = new DataView(ab);
+  var blob = new Blob([dataView], {type: "image/png"});
+  var DOMURL = self.URL || self.webkitURL || self;
+  var newurl = DOMURL.createObjectURL(blob);
+
+  var img = '<img src="'+newurl+'">';
+  d3.select("#img").html(img);
+}
+
+
 
 $(document).ready(function() {
   var container = createSVG();
   // var chart_bars = createChartBars(container);
   createChart(container);
+  enableSaveButton();
 });
